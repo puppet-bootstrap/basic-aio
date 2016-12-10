2c880f5ebe256c7a1ee029885dd397bd0117de01
James Evans <james@clark-evans.com>
2016-12-10 13:42:06 -0600

    Add hiera module to bootstrap.pp

--- bootstrap.pp
+++ bootstrap.pp
@@ -2,4 +2,8 @@ node puppet.vagrant {
   class { 'r10k':
     remote => 'https://github.com/puppet-bootstrap/basic-aio.git'
   }
+
+  class {'hiera':
+        hierarchy => ["nodes/%{::trusted.certname}",'common'],
+  }
 }
