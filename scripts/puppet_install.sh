ea2f8226800775ef84df938b7b5030fb4dcc12b4
James Evans <james@clark-evans.com>
2016-12-11 18:57:18 -0600

    Use puppet to apply configuration to puppetserver
    Install hiera module for use in bootstrap
    Wait 30 seconds for puppetserver to stabilize
    Run puppet agent to manage server configuration

--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -10,8 +10,22 @@ echo "192.168.32.5  puppet.vagrant  puppet" >> /etc/hosts
 # Source the profile to get the path
 . /etc/profile.d/puppet-agent.sh
 puppet module install puppet/r10k
+puppet module install puppet/hiera
 puppet apply /vagrant/bootstrap.pp
+
+# Deploy the control repo with the newly installed r10k
 r10k deploy environment -pv
+
+# Enable autosigning nodes in the .vagrant domain
+echo '*.vagrant' > /etc/puppetlabs/puppet/autosign.conf
 
 # Start the puppet master so further changes are under puppet control
 /bin/systemctl start puppetserver
+
+
+sleep 30
+
+# If puppet successfully applied changes, it returns 2.
+# Vagrant sees a non-zero return code as a failure. If puppet returns 2,
+# return a zero so vagrant doesn't report an error
+puppet agent -t || [ $? -eq 2 ] && /bin/true
