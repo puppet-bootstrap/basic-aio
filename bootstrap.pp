c528d122c7c068f202151b84bf4f874926eb88ae
James Evans <james@clark-evans.com>
2016-12-11 20:40:46 -0600

    Fix puppet-lint warnings

--- bootstrap.pp
+++ bootstrap.pp
@@ -1,9 +1,9 @@
-node puppet.vagrant {
+node 'puppet.vagrant' {
   class { 'r10k':
     remote => 'https://github.com/puppet-bootstrap/basic-aio.git'
   }
 
   class {'hiera':
-        hierarchy => ["nodes/%{::trusted.certname}",'common'],
+        hierarchy => ['nodes/%{::trusted.certname}','common'],
   }
 }
