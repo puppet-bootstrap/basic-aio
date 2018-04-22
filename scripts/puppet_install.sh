df1d0815ec07d23db685bd9465d7dfa3431a3ddc
Steven Pritchard <steven.pritchard@gmail.com>
2018-04-22 16:58:33 -0500

    Explicitly wait for cert for 2 minutes (which should be the default).

--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -12,6 +12,6 @@ puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
 # If puppet successfully applied changes, it returns 2.
 # Vagrant sees a non-zero return code as a failure. If puppet returns 2,
 # return a zero so vagrant doesn't report an error
-puppet agent -t --waitforcert || [ $? -eq 2 ] && /bin/true
+puppet agent -t --waitforcert 120 || [ $? -eq 2 ] && /bin/true
 
 su -- vagrant -c 'mco choria request_cert'
