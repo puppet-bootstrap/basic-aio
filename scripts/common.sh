b10280f724edd3f2ee260709e69fe2fc10cbe1a3
Steven Pritchard <steven.pritchard@gmail.com>
2018-04-22 16:41:07 -0500

    Move most configuration into role & profile modules.

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-#rpm -ihv https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
-yum -y install vim
-
 if [ -f /etc/sysconfig/network-scripts/ifcfg-eth1 ] ; then
     sed -i \
         -e '/^NM_CONTROLLED=/d;$aNM_CONTROLLED=yes' \
--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -2,26 +2,16 @@
 
 set -e
 
-yum -y install puppetserver
-
 # Bootstrap the puppet module configuration
 # Source the profile to get the path
 . /etc/profile.d/puppet-agent.sh
 puppet module install puppet/r10k
 puppet apply /vagrant/bootstrap.pp
-
-# Enable autosigning nodes in the .vagrant domain
-echo '*.vagrant' > /etc/puppetlabs/puppet/autosign.conf
-echo 'vagrant.mcollective' >> /etc/puppetlabs/puppet/autosign.conf
-
-# Start the puppet master so further changes are under puppet control
-puppet resource service puppetserver ensure=running enable=true
-
-sleep 30
+puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
 
 # If puppet successfully applied changes, it returns 2.
 # Vagrant sees a non-zero return code as a failure. If puppet returns 2,
 # return a zero so vagrant doesn't report an error
-puppet agent -t || [ $? -eq 2 ] && /bin/true
+puppet agent -t --waitforcert || [ $? -eq 2 ] && /bin/true
 
 su -- vagrant -c 'mco choria request_cert'
