407b02db4ba727171162b7555bb7c65a3b291430
Steven Pritchard <steven.pritchard@gmail.com>
2019-11-28 11:39:33 -0600

    Explicitly include git class to work around change in r10k class.

--- bootstrap.pp
+++ bootstrap.pp
@@ -1,9 +1,9 @@
 node 'puppet.vagrant' {
-  class { 'r10k':
+  class { 'git': }
+  -> class { 'r10k':
     remote => 'file:///vagrant/'
   }
-
-  exec { 'r10k deploy environment -pv':
+  -> exec { 'r10k deploy environment -pv':
     path    => '/opt/puppetlabs/bin:/bin:/usr/bin:/sbin:/usr/sbin',
     creates => '/etc/puppetlabs/code/environments/production/Puppetfile',
   }
