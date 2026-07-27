dad2e319727d755b95d9b1b6ce97f3915a10db92
Steven Pritchard <steven.pritchard@gmail.com>
2026-07-27 09:23:00 -0500

    Disable exec_idempotency check for required exec in plan

--- site/role/plans/init.pp
+++ site/role/plans/init.pp
@@ -77,7 +77,7 @@ plan role (
     -> class { 'r10k':
       remote => $control_repo,
     }
-    -> exec { 'r10k deploy environment -pv':
+    -> exec { 'r10k deploy environment -pv': # lint:ignore:exec_idempotency
       path => '/opt/puppetlabs/bin:/bin:/usr/bin:/sbin:/usr/sbin',
       # Always run this command to ensure that the environment is deployed.
     }
