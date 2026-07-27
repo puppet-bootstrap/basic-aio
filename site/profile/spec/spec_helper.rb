2b8da6af3d68764eb74cb0d93dcd56d6fb073ddd
Steven Pritchard <steven.pritchard@gmail.com>
2026-07-27 09:29:30 -0500

    Drop hiera from profile spec tests

--- site/profile/spec/spec_helper.rb
+++ site/profile/spec/spec_helper.rb
@@ -32,7 +32,6 @@ end
 
 RSpec.configure do |c|
   c.default_facts = default_facts
-  c.hiera_config = '../../hiera.yaml'
   c.before :each do
     # set to strictest setting for testing
     # by default Puppet runs at warning level
